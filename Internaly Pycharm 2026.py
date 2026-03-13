a90a236c12ca038b4f6e6b74348358d5d31f0088
xaviergardner <23014@wc.schoolnz>
2026-03-13 13:54:25 +1300

    Initial re-upload

---
+++ Internaly Pycharm 2026.py
@@ -0,0 +1,97 @@
+#Start 10/3/2026
+#Xavier Gardner Internal
+#-----------------------
+
+#Library Imports:
+#----------------
+import time
+import random
+
+#loop for while loops ect
+loop = True
+
+#Constants:
+#----------
+EXPEL_TIME = 12
+CORTISOL_SPIKE_GAME_OVER = 100
+KEY_ESCAPE_GATE = 1
+tower_block_level_1_pass = 1
+tower_block_level_2_pass = 1
+tower_block_level_3_pass = 1
+tower_block_level_4_pass = 1
+tower_block_level_5_pass = 1
+tower_block_level_6_gate_key = 1
+
+#Variables:
+#----------
+player_time = 5.5
+player_cortisol = 0
+player_key = 0 # later on if player_key = 1 then KEY_ESCAPE_GATE = True
+name = ""
+level_1_pass_desicion = ""
+
+#Functions:
+#----------
+def introduction():
+    slow_text("Before you play the best game of your life I need to know 1 thing from you...")
+    print()
+    #time.sleep(1)
+    name = input("what is your name: ")
+    print()
+    #time.sleep(1)
+    slow_text(f"Very well then {name}.")
+    print()
+    time.sleep(2)
+    slow_text("First of all you will need to understand the situation you are placed in...")
+    print()
+    time.sleep(1)
+    slow_text("The current time is 5:30PM.")
+    print()
+    slow_text("You slept through last period and the teacher left you behind...")
+    print()
+    slow_text("The gates are closed, you are trapped inside the school...")
+    print()
+    slow_text("Be careful as every move costs you time...")
+    slow_text("Make your way to the very top of the tower block where the almighty key is stored so you can unlock the school gate before it clocks midnight...")
+    print()
+    time.sleep(1)
+    slow_text(f"Good luck {name}.")
+    print()
+    print()
+    print()
+    time.sleep(3)
+
+#levels
+def tower_block():
+    slow_text("You wake up in first floor of tower block.")
+    print()
+    slow_text("You must find a elevator pass to the second floor,")
+
+def tower_block_level_1():
+    print()
+    time.sleep(1)
+    slow_text("you are inside a tech classroom, quickly search the class for a pass before you are found...")
+
+def tower_block_level_2():
+    print()
+    ptint()
+def tower_block_level_3():
+    print()
+def tower_block_level_4():
+    print()
+def tower_block_level_5():
+    print()
+
+def tower_block_level_6():
+    print()
+
+#function for printed text individually types a letter at a time. Speed will be set to 0.02 when finished
+def slow_text(text: str, speed = 10000000.02):
+    for letter in text:
+        print(letter, end = '')
+        time.sleep(0.001/speed)
+
+#Main Loop:
+#----------
+introduction()
+tower_block()
