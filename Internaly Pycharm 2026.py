7d401c3fca8682c14881f763bbebea7b6727a5d3
23104 <23104@wc.school@gmail.com>
2026-03-16 15:11:46 +1300

    16/03 re-uploading

--- Internaly Pycharm 2026.py
+++ Internaly Pycharm 2026.py
@@ -13,7 +13,6 @@ loop = True
 #Constants:
 #----------
 EXPEL_TIME = 12
-CORTISOL_SPIKE_GAME_OVER = 100
 KEY_ESCAPE_GATE = 1
 tower_block_level_1_pass = 1
 tower_block_level_2_pass = 1
@@ -32,6 +31,7 @@ level_1_pass_desicion = ""
 
 #Functions:
 #----------
+
 def introduction():
     slow_text("Before you play the best game of your life I need to know 1 thing from you...")
     print()
@@ -51,7 +51,8 @@ def introduction():
     print()
     slow_text("The gates are closed, you are trapped inside the school...")
     print()
-    slow_text("Be careful as every move costs you time...")
+    slow_text("Be careful as every move costs you time and can increase your cortisol...")
+    print()
     slow_text("Make your way to the very top of the tower block where the almighty key is stored so you can unlock the school gate before it clocks midnight...")
     print()
     time.sleep(1)
@@ -60,6 +61,22 @@ def introduction():
     print()
     print()
     time.sleep(3)
+
+def cortisol_check():
+    print()
+    if player_cortisol >= 100:
+        slow_text(f"{name}, Your cortisol levels exceeded the limit and you pass out.")
+        print()
+        time.sleep(2)
+        slow_text("You wake up in Mr Denham's office...")
+        print()
+        slow_text("I am not angry, I am just dissapointed, you are expelled!!!")
+        exit()
+    else:
+        print()
+        slow_text(f"You have a cortisol level of {player_cortisol}, be careful, do not get to a level of 100 cortisol...")
+        print()
+
 
 #levels
 def tower_block():
@@ -74,7 +91,6 @@ def tower_block_level_1():
 
 def tower_block_level_2():
     print()
-    ptint()
 def tower_block_level_3():
     print()
 def tower_block_level_4():
@@ -86,7 +102,7 @@ def tower_block_level_6():
     print()
 
 #function for printed text individually types a letter at a time. Speed will be set to 0.02 when finished
-def slow_text(text: str, speed = 10000000.02):
+def slow_text(text: str, speed = 10000.02):
     for letter in text:
         print(letter, end = '')
         time.sleep(0.001/speed)
@@ -95,3 +111,4 @@ def slow_text(text: str, speed = 10000000.02):
 #----------
 introduction()
 tower_block()
+cortisol_check()
