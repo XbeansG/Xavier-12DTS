742f218e0b48812a78e5525264a3b49db65acad3
xaviergardner <23014@wc.schoolnz>
2026-03-18 09:36:30 +1300

    re-upload 18 MAR

--- Internaly Pycharm 2026.py
+++ Internaly Pycharm 2026.py
@@ -12,32 +12,43 @@ loop = True
 
 #Constants:
 #----------
-EXPEL_TIME = 12
 KEY_ESCAPE_GATE = 1
 tower_block_level_1_pass = 1
 tower_block_level_6_gate_key = 1
 
 #Variables:
 #----------
-player_time = 5.5
+player_time = 5 #expel time = 12
 player_cortisol = 0
 player_key = 0 # later on if player_key = 1 then KEY_ESCAPE_GATE = True
 name = ""
 level_1_pass_desicion = ""
+start_menu_choice = ""
+display_updated_status = ""
 
 #Functions:
 #----------
 def start_menu():
     loop_control = True
+    slow_text("====WELLINGTON COLLEGE ESCAPE=====")
+    print()
+    print()
+    #time.sleep(1)
+
+    slow_text("Before you play the best game of your life I need to know 1 thing from you...")
+    print()
+    #time.sleep(1)
+    name = input("what is your name: ")
+    #time.sleep(1)
+    print()
+    #time.sleep(1)
+    slow_text(f"Very well then {name}.")
+    #time.sleep(3)
+    print()
+    print()
 
     while loop_control:
         try:
-            slow_text("====WELLINGTON COLLEGE ESCAPE=====")
-            print()
-            print()
-            print()
-            time.sleep(1)
-
             slow_text("1. Start (Enter 1)")
             print()
             print()
@@ -49,6 +60,7 @@ def start_menu():
             slow_text("3. Quit (Enter 3) ")
             print()
             print()
+            #time.sleep(1)
 
             start_menu_choice = int(input("Enter your choice: "))
 
@@ -69,21 +81,61 @@ def start_menu():
             slow_text("Enter an integer, ever 1, 2 or 3")
             print()
 
+def status():
+    loop_control = True
+    while loop_control:
+        try:
+            slow_text("1. Cortisol level (Enter 1)")
+            print()
+            print()
+
+            slow_text("2. Current time (Enter 2)")
+            print()
+            print()
+
+            slow_text("3. Map (Enter 3) ")
+            print()
+            print()
+            #time.sleep(1)
+            slow_text("3. Current inventory (Enter 4) ")
+            print()
+            print()
+
+            display_updated_status = int(input("Enter your choice: "))
+
+            if display_updated_status == 1:
+                cortisol_check()
+                print()
+                print()
+                loop_control = False
+
+            elif display_updated_status == 2:
+                slow_text(f"The current time is {player_time}PM.")
+                print()
+                print()
+                loop_control = False
+
+            elif display_updated_status == 3:
+                exit()
+
+            elif display_updated_status == 4:
+                exit()
+            else:
+                slow_text("You have to enter 1, 2, 3 or 4")
+                print()
+
+        except ValueError:
+            slow_text("Enter an integer, ever 1, 2, 3 or 4")
+            print()
+
+
 
 
 def introduction():
-    slow_text("Before you play the best game of your life I need to know 1 thing from you...")
-    print()
-    #time.sleep(1)
-    name = input("what is your name: ")
-    print()
-    #time.sleep(1)
-    slow_text(f"Very well then {name}.")
-    print()
-    time.sleep(2)
+    #time.sleep(2)
     slow_text("First of all you will need to understand the situation you are placed in...")
     print()
-    time.sleep(1)
+    #time.sleep(1)
     slow_text("The current time is 5:30PM.")
     print()
     slow_text("You slept through last period and the teacher left you behind...")
@@ -94,19 +146,19 @@ def introduction():
     print()
     slow_text("Make your way to the very top of the tower block where the almighty key is stored so you can unlock the school gate before it clocks midnight...")
     print()
-    time.sleep(1)
+    #time.sleep(1)
     slow_text(f"Good luck {name}.")
     print()
     print()
     print()
-    time.sleep(3)
+    #time.sleep(3)
 
 def cortisol_check():
     print()
     if player_cortisol >= 100:
         slow_text(f"{name}, Your cortisol levels exceeded the limit and you pass out.")
         print()
-        time.sleep(2)
+        #time.sleep(2)
         slow_text("You wake up in Mr Denham's office...")
         print()
         slow_text("I am not angry, I am just dissapointed, you are expelled!!!")
@@ -122,21 +174,19 @@ def tower_block():
     slow_text("You wake up in first floor of tower block.")
     print()
     slow_text("You must find a elevator pass to the second floor,")
-
-def tower_block_level_1():
     print()
-    time.sleep(1)
+    #time.sleep(1)
     slow_text("you are inside a tech classroom, quickly search the class for a pass before you are found...")
 
 #function for printed text individually types a letter at a time. Speed will be set to 0.02 when finished
-def slow_text(text: str, speed = 10000.02):
+def slow_text(text: str, speed = 0.02):
     for letter in text:
         print(letter, end = '')
         time.sleep(0.001/speed)
 
 #Main Loop:
 #----------
+status()
 start_menu()
-introduction()
 tower_block()
 
